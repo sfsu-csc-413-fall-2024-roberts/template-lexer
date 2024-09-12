4b1ac6fa19f7c3b2314248aeb8076165ce1f2bf0
John Roberts <roberts.john@gmail.com>
2024-09-11 18:51:35 -0700

    starting over

--- src/lexer/ILexer.java
+++
@@ -1,5 +0,0 @@
-package lexer;
-
-public interface ILexer {
-
-}
--- tools/CompilerToolException.java
+++
@@ -1,7 +0,0 @@
-package tools;
-
-public class CompilerToolException extends Exception {
-    public CompilerToolException(String message) {
-        super(message);
-    }
-}
