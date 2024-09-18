1577c27871f3efb2753c13eb3d51a23420f8d77a
John Roberts <roberts.john@gmail.com>
2024-09-18 16:06:22 -0700

    updated source file reader implementation (reverted to character by character for efficiency and ease of implementation)

--- src/lexer/readers/SourceFileReader.java
+++ src/lexer/readers/SourceFileReader.java
@@ -9,64 +9,66 @@ import lexer.LexicalException;
 
 public class SourceFileReader implements IReader {
 
-    private BufferedReader reader;
+  private BufferedReader reader;
 
-    private String currentLine;
-    private int currentLineNumber;
-    private int currentColumn;
+  private int currentLineNumber;
+  private int currentColumn;
+  private char lastCharacterRead;
 
-    public SourceFileReader(String fileName) throws FileNotFoundException, LexicalException {
-        this(new BufferedReader(new FileReader(fileName)));
+  public SourceFileReader(String fileName) throws FileNotFoundException, LexicalException {
+    this(new BufferedReader(new FileReader(fileName)));
+  }
+
+  public SourceFileReader(BufferedReader reader) throws LexicalException {
+    this.reader = reader;
+
+    this.currentLineNumber = 0;
+    this.currentColumn = -1;
+    this.lastCharacterRead = ' ';
+  }
+
+  @Override
+  public void close() throws Exception {
+    this.reader.close();
+  }
+
+  @Override
+  public char read() throws LexicalException {
+    if (this.lastCharacterRead == '\n') {
+      this.currentLineNumber++;
+      this.currentColumn = -1;
     }
 
-    public SourceFileReader(BufferedReader reader) throws LexicalException {
-        this.reader = reader;
-        this.currentLineNumber = -1;
+    this.lastCharacterRead = this.getNextCharacter();
+    this.currentColumn++;
 
-        this.getNextLine();
+    return this.lastCharacterRead;
+  }
+
+  private char getNextCharacter() throws LexicalException {
+    int next;
+
+    try {
+      next = this.reader.read();
+
+      if (next == -1) {
+        return '\0';
+      }
+    } catch (IOException e) {
+      throw new LexicalException(String.format("Error reading file (column: %d, line: %d)",
+          this.currentColumn, this.currentLineNumber));
     }
 
-    private String getNextLine() throws LexicalException {
-        try {
-            this.currentLine = this.reader.readLine();
-            this.currentLineNumber++;
-            this.currentColumn = -1;
+    return (char) next;
+  }
 
-            return this.currentLine;
-        } catch (IOException e) {
-            throw new LexicalException(String.format("Invalid attempt to read line %d", this.currentLine + 1));
-        }
-    }
+  @Override
+  public int getColumn() {
+    return this.currentColumn;
+  }
 
-    @Override
-    public void close() throws Exception {
-        this.reader.close();
-    }
-
-    @Override
-    public char read() throws LexicalException {
-        if (this.currentLine == null) {
-            return '\0';
-        }
-
-        if (this.currentColumn + 1 == this.currentLine.length()) {
-            String line = this.getNextLine();
-
-            if (line == null) {
-                return '\0';
-            }
-        }
-
-        return this.currentLine.charAt(++this.currentColumn);
-    }
-
-    @Override
-    public int getColumn() {
-        return this.currentColumn;
-    }
-
-    @Override
-    public int getLineNumber() {
-        return this.currentLineNumber;
-    }
+  @Override
+  public int getLineNumber() {
+    return this.currentLineNumber;
+  }
 }
--- tests/lexer/readers/SourceFileReaderTest.java
+++ tests/lexer/readers/SourceFileReaderTest.java
@@ -11,98 +11,103 @@ import lexer.readers.SourceFileReader;
 
 public class SourceFileReaderTest {
 
-    private BufferedReader createSourceReader(String source) {
-        return new BufferedReader(new StringReader(source));
+  private BufferedReader createSourceReader(String source) {
+    return new BufferedReader(new StringReader(source));
+  }
+
+  @Test
+  public void testEmptyFile() throws Exception {
+    BufferedReader source = createSourceReader("");
+
+    try (SourceFileReader reader = new SourceFileReader(source)) {
+      assertEquals('\0', reader.read());
     }
+  }
 
-    @Test
-    public void testEmptyFile() throws Exception {
-        BufferedReader source = createSourceReader("");
+  @Test
+  public void testSingleCharacterFile() throws Exception {
+    BufferedReader source = createSourceReader("b");
 
-        try (SourceFileReader reader = new SourceFileReader(source)) {
-            assertEquals('\0', reader.read());
-        }
+    try (SourceFileReader reader = new SourceFileReader(source)) {
+      assertEquals('b', reader.read());
+      assertEquals('\0', reader.read());
     }
+  }
 
-    @Test
-    public void testSingleCharacterFile() throws Exception {
-        BufferedReader source = createSourceReader("b");
+  @Test
+  public void testMultilineFile() throws Exception {
+    String content = "program {\n  int i\n}";
+    BufferedReader source = createSourceReader(content);
 
-        try (SourceFileReader reader = new SourceFileReader(source)) {
-            assertEquals('b', reader.read());
-            assertEquals('\0', reader.read());
-        }
+    try (SourceFileReader reader = new SourceFileReader(source)) {
+      for (int index = 0; index < content.length(); index++) {
+        // This is done because our Reader strips the newlines off of the end
+        // of the line, and is not expected to return it to the Lexer
+
+        assertEquals(content.charAt(index), reader.read());
+      }
+
+      assertEquals('\0', reader.read());
     }
+  }
 
-    @Test
-    public void testMultilineFile() throws Exception {
-        String content = "program {\n  int i\n}";
-        BufferedReader source = createSourceReader(content);
+  @Test
+  public void testGetColumn() throws Exception {
+    String content = "compsci";
+    BufferedReader source = createSourceReader(content);
 
-        try (SourceFileReader reader = new SourceFileReader(source)) {
-            for (int index = 0; index < content.length(); index++) {
-                // This is done because our Reader strips the newlines off of the end
-                // of the line, and is not expected to return it to the Lexer
-                if (content.charAt(index) == '\n') {
-                    continue;
-                }
-
-                assertEquals(content.charAt(index), reader.read());
-            }
-
-            assertEquals('\0', reader.read());
-        }
+    try (SourceFileReader reader = new SourceFileReader(source)) {
+      for (int index = 0; index < content.length(); index++) {
+        reader.read();
+        assertEquals(index, reader.getColumn());
+      }
     }
+  }
 
-    @Test
-    public void testGetColumn() throws Exception {
-        String content = "compsci";
-        BufferedReader source = createSourceReader(content);
+  @Test
+  public void testGetColumnMultiline() throws Exception {
+    String content = "a\nbc\nd";
+    BufferedReader source = createSourceReader(content);
 
-        try (SourceFileReader reader = new SourceFileReader(source)) {
-            for (int index = 0; index < content.length(); index++) {
-                reader.read();
-                assertEquals(index, reader.getColumn());
-            }
-        }
+    try (SourceFileReader reader = new SourceFileReader(source)) {
+      reader.read(); // a
+      assertEquals(0, reader.getColumn());
+      reader.read(); // \n
+      assertEquals(1, reader.getColumn());
+
+      reader.read(); // b
+      assertEquals(0, reader.getColumn());
+      reader.read(); // c
+      assertEquals(1, reader.getColumn());
+      reader.read(); // \n
+      assertEquals(2, reader.getColumn());
+
+      reader.read(); // d
+      assertEquals(0, reader.getColumn());
     }
+  }
 
-    @Test
-    public void testGetColumnMultiline() throws Exception {
-        String content = "a\nbc\nd";
-        BufferedReader source = createSourceReader(content);
+  @Test
+  public void testGetLineNumber() throws Exception {
+    String content = "a\nbc\nd";
+    BufferedReader source = createSourceReader(content);
 
-        try (SourceFileReader reader = new SourceFileReader(source)) {
-            reader.read();
-            assertEquals(0, reader.getColumn());
+    try (SourceFileReader reader = new SourceFileReader(source)) {
+      reader.read(); // a
+      assertEquals(0, reader.getLineNumber());
+      reader.read(); // \n
+      assertEquals(0, reader.getLineNumber());
 
-            reader.read();
-            assertEquals(0, reader.getColumn());
-            reader.read();
-            assertEquals(1, reader.getColumn());
+      reader.read(); // b
+      assertEquals(1, reader.getLineNumber());
+      reader.read(); // c
+      assertEquals(1, reader.getLineNumber());
+      reader.read(); // \n
+      assertEquals(1, reader.getLineNumber());
 
-            reader.read();
-            assertEquals(0, reader.getColumn());
-        }
+      reader.read(); // d
+      assertEquals(2, reader.getLineNumber());
     }
-
-    @Test
-    public void testGetLineNumber() throws Exception {
-        String content = "a\nbc\nd";
-        BufferedReader source = createSourceReader(content);
-
-        try (SourceFileReader reader = new SourceFileReader(source)) {
-            reader.read();
-            assertEquals(0, reader.getLineNumber());
-
-            reader.read();
-            assertEquals(1, reader.getLineNumber());
-            reader.read();
-            assertEquals(1, reader.getLineNumber());
-
-            reader.read();
-            assertEquals(2, reader.getLineNumber());
-        }
-    }
+  }
 
 }
